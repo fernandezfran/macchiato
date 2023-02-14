1426ae062ceee4374ac3c0150e177c7249cea5cf
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-02-14 11:03:20 -0300

    test parametrize in a class instead in both functions

--- tests/test_chemical_shift.py
+++ tests/test_chemical_shift.py
@@ -33,51 +33,44 @@ import pytest
         ("Li240Si64"),
     ],
 )
-def test_fit(structure, request):
-    """Test the fit attributes of the ChemicalShiftSpectra."""
-    structure = request.getfixturevalue(structure)
+class TestChemicalShiftSpectra:
+    """Test the ChemicalShiftSpectra fitting."""
 
-    css = ChemicalShiftSpectra(
-        structure["trajectory"],
-        "Li",
-        "Si",
-        3.4,
-        3.0,
-        {"bonded": 18.0, "isolated": 6.0},
-    )
+    def test_fit(self, structure, request):
+        """Test the fit attributes of the ChemicalShiftSpectra."""
+        structure = request.getfixturevalue(structure)
 
-    css.fit(None)
+        css = ChemicalShiftSpectra(
+            structure["trajectory"],
+            "Li",
+            "Si",
+            3.4,
+            3.0,
+            {"bonded": 18.0, "isolated": 6.0},
+        )
 
-    np.testing.assert_array_almost_equal(css.bonded_, structure["bonded"])
-    np.testing.assert_array_almost_equal(css.isolated_, structure["isolated"])
+        css.fit(None)
 
+        np.testing.assert_array_almost_equal(css.bonded_, structure["bonded"])
+        np.testing.assert_array_almost_equal(
+            css.isolated_, structure["isolated"]
+        )
 
-@pytest.mark.parametrize(
-    ("structure"),
-    [
-        ("Li13Si64"),
-        ("Li36Si64"),
-        ("Li57Si64"),
-        ("Li128Si64"),
-        ("Li210Si64"),
-        ("Li240Si64"),
-    ],
-)
-def test_fit_predict(structure, request):
-    """Test the fit_predict of the ChemicalShiftSpectra."""
-    structure = request.getfixturevalue(structure)
+    def test_fit_predict(self, structure, request):
+        """Test the fit_predict of the ChemicalShiftSpectra."""
+        structure = request.getfixturevalue(structure)
 
-    css = ChemicalShiftSpectra(
-        structure["trajectory"],
-        "Li",
-        "Si",
-        3.4,
-        3.0,
-        {"bonded": 18.0, "isolated": 6.0},
-    )
+        css = ChemicalShiftSpectra(
+            structure["trajectory"],
+            "Li",
+            "Si",
+            3.4,
+            3.0,
+            {"bonded": 18.0, "isolated": 6.0},
+        )
 
-    contributions = css.fit_predict(None)
+        contributions = css.fit_predict(None)
 
-    np.testing.assert_array_almost_equal(
-        contributions, structure["contributions"]
-    )
+        np.testing.assert_array_almost_equal(
+            contributions, structure["contributions"]
+        )
